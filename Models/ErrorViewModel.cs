493ea933c0d7a26a71d7e8ddd2d655627700e5f1
henrickrafael <nevesrafel@hotmail.com>
2023-04-15 18:06:39 -0300

    initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace AspCoreProject.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
